84f944c1b3daba39e613a36958708c232eaf31b4
Saurabh Joshi <64773452+j-saurabh@users.noreply.github.com>
2025-06-22 01:30:54 +0530

    Create main.tf

---
+++ resource-group2/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+
+  tags = {
+    Owner = "Saurabh.Joshi"
+  }
+}
